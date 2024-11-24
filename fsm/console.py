134dec3e991be7474243b8f820618a4373b40dcf
Paolo Bignardi <paolo.bigno@gmail.com>
2024-11-24 18:07:42 +0100

    feat: method to pretty print array

--- fsm/console.py
+++ fsm/console.py
@@ -1,3 +1,4 @@
+from libtmux import Session
 from rich.console import Console
 from rich.style import Style
 from rich.theme import Theme
@@ -6,6 +7,7 @@ theme = Theme(
     {
         "error": Style(color="red", bold=True),
         "warning": Style(color="yellow", italic=True),
+        "info": Style(color="cyan"),
     }
 )
 console = Console(theme=theme)
@@ -21,3 +23,22 @@ def print_warning(message):
 
 def pretty_print(text):
     console.print(text)
+
+
+def print_list(array: list, header: str = "", repr: bool = False):
+    if array and header:
+        console.print(header, style="info")
+
+    for a in array:
+        if repr:
+            console.print(f" - {a!r}")
+        else:
+            console.print(f" - {a}")
+
+
+def confirm(prompt):
+    console.print(f"{prompt}")
+
+
+def mark_as_ssh(text):
+    return f"{text} ([green]SSH[/green])"
